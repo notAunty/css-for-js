d2546256675b77455296d7e98a34889235ece6c8
aso <wid180039@siswa.um.edu.my>
2022-03-25 23:05:12 +0800

    done ex3: Shell

--- sole-and-ankle/src/components/ShoeIndex/ShoeIndex.js
+++ sole-and-ankle/src/components/ShoeIndex/ShoeIndex.js
@@ -42,13 +42,22 @@ const ShoeIndex = ({ sortId, setSortId }) => {
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  gap: 32px;
+`;
 
 const LeftColumn = styled.div``;
 
-const MainColumn = styled.div``;
+const MainColumn = styled.div`
+  flex: 1;
+`;
 
-const Header = styled.header``;
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+`;
 
 const Title = styled.h2`
   font-size: 1.5rem;
