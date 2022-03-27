9f58ee9b85c62df8db3b7615cc69f7b179b4cf1a
aso <wid180039@siswa.um.edu.my>
2022-03-27 10:21:14 +0800

    mobile menu

--- sole-and-ankle-revisited/src/components/MobileMenu/MobileMenu.js
+++ sole-and-ankle-revisited/src/components/MobileMenu/MobileMenu.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components/macro';
-import { DialogOverlay, DialogContent } from '@reach/dialog';
+import React from "react";
+import styled from "styled-components/macro";
+import { DialogOverlay, DialogContent } from "@reach/dialog";
 
-import { QUERIES } from '../../constants';
+import { COLORS, QUERIES, WEIGHTS } from "../../constants";
 
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import UnstyledButton from "../UnstyledButton";
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
   if (!isOpen) {
@@ -15,23 +15,85 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
   }
 
   return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
+    <Overlay isOpen={isOpen} onDismiss={onDismiss}>
+      <Wrapper>
+        <CloseButton onClick={onDismiss}>
+          <Icon id="close">
+            <VisuallyHidden>Dismiss menu</VisuallyHidden>
+          </Icon>
+        </CloseButton>
+        <MobileNav>
+          <a href="/sale">Sale</a>
+          <a href="/new">New&nbsp;Releases</a>
+          <a href="/men">Men</a>
+          <a href="/women">Women</a>
+          <a href="/kids">Kids</a>
+          <a href="/collections">Collections</a>
+        </MobileNav>
+        <MobileFooter>
+          <a href="/terms">Terms and Conditions</a>
+          <a href="/privacy">Privacy Policy</a>
+          <a href="/contact">Contact Us</a>
+        </MobileFooter>
+      </Wrapper>
+    </Overlay>
   );
 };
+
+const Overlay = styled(DialogOverlay)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: hsl(0deg 0% 0% / 0.5);
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Wrapper = styled(DialogContent)`
+  position: relative;
+  width: 85%;
+  background-color: ${COLORS.white};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 32px;
+`;
+
+const CloseButton = styled(UnstyledButton)`
+  align-self: flex-end;
+`;
+
+const MobileNav = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+
+  > a {
+    text-transform: uppercase;
+    text-decoration: none;
+    font-size: ${18 / 16}rem;
+    color: black;
+    font-weight: ${WEIGHTS.medium};
+
+    &:first-of-type {
+      color: ${COLORS.secondary};
+    }
+  }
+`;
+
+const MobileFooter = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+
+  > a {
+    font-size: ${12 / 16}rem;
+    font-weight: ${WEIGHTS.medium};
+    text-decoration: none;
+    color: hsl(210 5% 40%);
+  }
+`;
 
 export default MobileMenu;
