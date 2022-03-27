1259c2ecd2f6ebdefa8b7dda0da4266c6d57f6d4
aso <wid180039@siswa.um.edu.my>
2022-03-27 11:32:34 +0800

    done exe3 tweak main view

--- sole-and-ankle-revisited/src/components/ShoeIndex/ShoeIndex.js
+++ sole-and-ankle-revisited/src/components/ShoeIndex/ShoeIndex.js
@@ -1,40 +1,51 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { WEIGHTS } from '../../constants';
+import { MEDIA_QUERIES, WEIGHTS } from "../../constants";
 
-import Breadcrumbs from '../Breadcrumbs';
-import Select from '../Select';
-import Spacer from '../Spacer';
-import ShoeSidebar from '../ShoeSidebar';
-import ShoeGrid from '../ShoeGrid';
+import Breadcrumbs from "../Breadcrumbs";
+import Select from "../Select";
+import Spacer from "../Spacer";
+import ShoeSidebar from "../ShoeSidebar";
+import ShoeGrid from "../ShoeGrid";
+
+const BreadcrumbsComponents = () => {
+  return (
+    <Breadcrumbs>
+      <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+      <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+      <Breadcrumbs.Crumb href="/sale/shoes">Shoes</Breadcrumbs.Crumb>
+    </Breadcrumbs>
+  );
+};
 
 const ShoeIndex = ({ sortId, setSortId }) => {
   return (
     <Wrapper>
       <MainColumn>
         <Header>
-          <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
+          <TitleWrapper>
+            <TabletBreadcrumbsWrapper>
+              <BreadcrumbsComponents />
+            </TabletBreadcrumbsWrapper>
+            <Title>Running</Title>
+          </TitleWrapper>
+          <SelectWrapper>
+            <Select
+              label="Sort"
+              value={sortId}
+              onChange={(ev) => setSortId(ev.target.value)}
+            >
+              <option value="newest">Newest Releases</option>
+              <option value="price">Price</option>
+            </Select>
+          </SelectWrapper>
         </Header>
         <Spacer size={32} />
         <ShoeGrid />
       </MainColumn>
       <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
-        </Breadcrumbs>
+        <BreadcrumbsComponents />
         <Spacer size={42} />
         <ShoeSidebar />
       </LeftColumn>
@@ -51,6 +62,10 @@ const Wrapper = styled.div`
 
 const LeftColumn = styled.div`
   flex-basis: 248px;
+
+  @media screen and (${MEDIA_QUERIES.maxTablet}) {
+    display: none;
+  }
 `;
 
 const MainColumn = styled.div`
@@ -61,11 +76,35 @@ const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
+
+  @media screen and (${MEDIA_QUERIES.maxTablet}) {
+    align-items: center;
+  }
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 `;
 
 const Title = styled.h2`
   font-size: 1.5rem;
   font-weight: ${WEIGHTS.medium};
+`;
+
+const TabletBreadcrumbsWrapper = styled.div`
+  display: none;
+
+  @media screen and (${MEDIA_QUERIES.maxTablet}) {
+    display: revert;
+  }
+`;
+
+const SelectWrapper = styled.div`
+  @media screen and (${MEDIA_QUERIES.maxPhone}) {
+    display: none;
+  }
 `;
 
 export default ShoeIndex;
